e98eb6d016a8c86176c05835de6d85d6f8e878b2
formarfr <frank.formaro@pioneer.com>
2018-06-01 15:25:20 -0500

    updated spec array

--- spec/integrational/protractor-config/ci-variables-tfs.js
+++ spec/integrational/protractor-config/ci-variables-tfs.js
@@ -3,7 +3,7 @@ var env = require('../environment');
 exports.config = {
   seleniumAddress: env.seleniumAddress,
   framework: 'jasmine2',
-  specs: ['../protractor/angularjs-homepage-test.js'],
+  specs: ['../protractor/ci-variables-tfs-test.js'],
   plugins: [{
     path: '../../../index.js',
     screenshotPath: '.tmp/ci-variables-tfs',
